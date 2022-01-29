68dfa0fd5fb2ced9b27f0a5b61f97a8cee84fa61
CO-OK <1260999370@qq.com>
2022-01-29 17:25:12 +0800

    增加.gitignore文件

--- esp/ToyOs/build/CMakeFiles/Kernel.elf.dir/Kernel.c.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/Kernel.elf.dir/Kernel.c.o: \
-  /home/v/program/EFI/runOvmf/esp/ToyOs/Kernel.c \
-  /home/v/program/EFI/runOvmf/esp/ToyOs/Kernel.h \
-  /home/v/program/EFI/runOvmf/esp/ToyOs/Video.h \
-  /home/v/program/EFI/runOvmf/esp/ToyOs/Uefi.h
--- esp/ToyOs/build/CMakeFiles/Kernel.elf.dir/Video.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Kernel.elf.dir/Video.c.o: \
-  /home/v/program/EFI/runOvmf/esp/ToyOs/Video.c \
-  /home/v/program/EFI/runOvmf/esp/ToyOs/Video.h \
-  /home/v/program/EFI/runOvmf/esp/ToyOs/Uefi.h
